bf1720f606d3a0b5bb787f59d42415304d34cfd7
Sumeet Godse <sumeetgodse1997@gmail.com>
2021-05-23 20:12:19 +0530

    Delete src directory

--- src/reducers/mainreducer.js
+++
@@ -1,9 +0,0 @@
-import changeDifficulty from "./difficulty"
-
-import { combineReducers } from "redux"
-
-const rootReducer = combineReducers({
-    changeDifficulty
-})
-
-export default rootReducer
